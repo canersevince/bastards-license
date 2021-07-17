11927e8159ca9255c587b6d2cca620f30b41a330
Berk Özdemir <berkozdemir@Berks-MacBook-Pro.local>
2021-07-17 23:53:25 +0300

    deneme

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,15 +8,19 @@ export default function Home() {
     const isConnected = typeof account === "string" && !!library;
 
     return (
-        <div>
+        <div className="bg-black min-h-screen">
             <Head>
                 <title>YOURBASTARDYOURCALL - FUCK DMCA</title>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
             <div>
+                <p className="text-3xl text-center font-bold text-white py-8">YOUR BASTARD, YOUR CALL!</p>
+                <p className="text-xl text-center font-bold text-white py-8">IF YOU ARE GIVING HOME TO A BASTARD (EITHER V1 AND V2), YOU HOLD ALL COPYRIGHT AND IP RIGHTS OF IT. ACTUALLY WE WANNA GO BEYOND THAT AND MAKE IT MOST BASTARDOUS WAY POSSIBLE. SO NOW THERE IS A SMART CONTRACT </p>
+                <p className="text-xl text-center font-bold text-white py-8">ON THIS WEBSITE, YOU WILL BE ABLE TO  </p>
+
                 Providerlar <br/>
                 <select name="" id="">
-                    {providers.map(provider => <option>{JSON.stringify(provider)}</option>)}
+                    {providers.map(provider => <option>{provider["name"]}</option>)}
                 </select>
             </div>
         </div>
